b1834ab312598965b22fdd29fe46811a4c959ca5
Awa Diop <diopawa192@gmail.com>
2022-02-27 12:34:07 +0000

    us2 terminer

--- config/orm.php
+++ config/orm.php
@@ -1,15 +1,23 @@
 <?php 
 # cette page permet de faire la conversion comme le fichier json, on a besoin de convertir les objet json en tableau
+
+//recuperation des donnees
 function json_to_array(string $key){
     $json = file_get_contents(PATH_DB);
     $array = json_decode($json, true);
     return $array[$key];
 }
-$data = (json_to_array("users"));
+$data = (json_to_array("Questions"));
 
+$donne = [
+    "nom" => "ba",
+    "prenom" => "kali"
+] ;
+//Enregistrement et mise a jour des donnees
 // function array_to_json(string $key, array $data){
-//     $array = json_encode($data);
-//     return $array[$key];
+//      $array = json_encode($data);
+//     //  file_put_contents(PATH_DB);
+//      return $array[$key];
 // }
-// var_dump(array_to_json("users", $data));
+// var_dump(array_to_json("users", $donne));
 
