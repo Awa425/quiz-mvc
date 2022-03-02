fb7f43f0c469ee2e075afcc0db459771cd515fa1
Awa Diop <diopawa192@gmail.com>
2022-03-02 19:10:53 +0000

    ajout entete

--- templates/securite/login.html.php
+++ templates/securite/login.html.php
@@ -11,40 +11,66 @@
         <input type='hidden' name='action' value='$action'>";
     }
 ?>  
-<?=$errors['login'];?> 
 
-    <div class="container">
-        <form action="<?=WEB_ROOT?>" method="POST" class="form" id="form">
-            <div class="entete">
-                <h5>Login form</h5>
-                <p> <a href="#">X</a> </p>
-            </div>
-            <div class="form-control">
-            <?php vers("securite", "connexion") ?> 
+<!DOCTYPE html>
+<html lang="en">
 
-            <?php if(isset($errors['connexion'])){ ?>
-                <p style="color: red"> <?=$errors['connexion'];?> </p>
-            <?php } ?>   
-            </div>
-            
-            <div class="form-control">
-                <input id="email" name="login" type="text" placeholder="Login"> <br>
-                <small> </small>
-            </div> 
-            <div class="form-control">
-                <!-- <label for="password">Password</label> -->
-                <input id="password" name="password" type="password" placeholder="Password" > <br>
-                <small></small>
-            </div>
-            <div class="footer">
-                <button type="submit" value="connexion" name="connexion" disabled  id="btn">Connexion</button>
-                <a href="<?= WEB_ROOT."?controller=securite&action=inscription" ?>">Inscrivez-vous</a>
-            </div>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=WEB_ROOT."css".DIRECTORY_SEPARATOR."style.connexion.css"?>">
+    <title>Login</title>
+</head>
 
-        </form>
+<body>
+    <div class="header">
+        <i><img src="<?=PATH_VIEW."img".DIRECTORY_SEPARATOR."logo-QuizzSA.png"?>" alt=""></i>
+        <h4 style="width: 94%;">Le plaisir de jouer</h4>
     </div>
- 
-    <?php 
-    require_once(PATH_VIEW."include".DIRECTORY_SEPARATOR."footer.inc.html.php");
+    <div class="main">
+        <div class="container">
+            <form action="<?=WEB_ROOT?>" method="POST" class="form" id="form">
+                <div class="entete">
+                    <h5>Login form</h5>
+                    <p> <a href="#">X</a> </p>
+                </div>
+                <div class="form-control">
+                <?php vers("securite", "connexion") ?> 
 
-    ?>
+                <?php if(isset($errors['connexion'])){ ?>
+                    <p style="color: red"> <?=$errors['connexion'];?> </p>
+                <?php } ?>   
+                </div>
+                
+                <div class="form-control">
+                <div>
+                        <input id="email" name="login" type="text" placeholder="Login">
+                        <span></span>
+                </div> <br>
+                    <small> </small>
+                </div> 
+                <div class="form-control">
+                    <!-- <label for="password">Password</label> -->
+                    <div>
+                        <input id="password" name="password" type="password" placeholder="Password" >
+                        <span></span>
+                    </div> <br>
+                    <small></small>
+                </div>
+                <div class="footer">
+                    <button type="submit" value="connexion" name="connexion" disabled  id="btn">Connexion</button>
+                    <a href="<?= WEB_ROOT."?controller=securite&action=inscription" ?>" class="inscrire">S'inscrire pour jouer?</a>
+                </div>
+
+            </form>
+        </div>
+    </div>
+   
+ 
+    
+    <script src="<?=WEB_ROOT."js".DIRECTORY_SEPARATOR."script.js"?>"></script>
+</body>
+
+
+</html>
